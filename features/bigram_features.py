5805891eb2d7ede99b84aeaa96300271cbc05c51
Victor Zhong <victor@victorzhong.com>
2014-11-11 19:17:19 -0800

    wrap try catch to avoid rare unicode errors

--- features/bigram_features.py
+++ features/bigram_features.py
@@ -37,20 +37,24 @@ all_labels = set()
 
 example_idx = 0
 documents = []
-num_examples = 10000
+num_examples = 2000000
 
 for example in DataStreamer.load_from_bz2(raw_text):
   if example_idx >= num_examples:
     break
   if example_idx % 1000 == 0:
     print 'read', example_idx, 'examples'
-  example_idx += 1
 
   all_labels = all_labels.union(example.data['tags'])
-  code, noncode = extract_code_sections(example.data['body'], lower=True)
+  try:
+    code, noncode = extract_code_sections(example.data['body'], lower=True)
+  except Exception as e:
+    continue
 
   if not noncode:
     continue
+
+  example_idx += 1
   documents += [noncode]
 
 vectorizer = HashingVectorizer(ngram_range=(1,2), binary = True, stop_words='english')
