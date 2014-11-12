15c155f69ddc09a2aa13a73dc71849d7182e8903
Victor Zhong <victor@victorzhong.com>
2014-11-12 09:02:47 -0800

    try pickling instead

--- features/common.py
+++ features/common.py
@@ -1,15 +1,21 @@
 import numpy as np
+import cPickle as pickle
 from scipy.sparse import csr_matrix
 from bs4 import BeautifulSoup
 
 def save_sparse_csr(filename,array):
-    np.savez(filename,data = array.data ,indices=array.indices,
-             indptr =array.indptr, shape=array.shape )
+  with open(filename + '.pickle', 'wb') as f:
+    pickle.dump(array, f, protocol=pickle.HIGHEST_PROTOCOL)
+    # np.savez(filename,data = array.data ,indices=array.indices,
+    #          indptr =array.indptr, shape=array.shape )
 
 def load_sparse_csr(filename):
-    loader = np.load(filename)
-    return csr_matrix((  loader['data'], loader['indices'], loader['indptr']),
-                         shape = loader['shape'])
+  with open(filename + '.pickle', 'rb') as f:
+    mat = pickle.load(f)
+  return mat
+  # loader = np.load(filename)
+  # return csr_matrix((  loader['data'], loader['indices'], loader['indptr']),
+  #                      shape = loader['shape'])
 
 
 def extract_code_sections(mixed):
