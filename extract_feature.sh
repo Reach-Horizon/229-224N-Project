97a38c6f43d41a6749448d2b836bcbeeb3cb0b2e
Victor Zhong <victor@victorzhong.com>
2014-11-18 17:37:53 -0800

    bash script for basic feature extraction

---
+++ extract_feature.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+top_labels=10
+min_count=100
+max_count=200
+cutoff=2
+
+set prefix=top${top_labels}min${min_count}
+
+mkdir experiments
+
+echo "subsampling data for top ${top_labels} labels and minimum ${min_count} maximum ${max_count} exampels per label"
+python util/subsample.py full_data/tags.count.json full_data/Train.csv.bz2 experiments/${prefix}.bz2 -n $top_labels --min_count $min_count --max_count $max_count
+
+echo "extracting features"
+python features/bigram_features.py experiments/${prefix}.bz2 experiments/${prefix} -c $cutoff
+
+echo "your files are at experiments/${prefix}.*"
