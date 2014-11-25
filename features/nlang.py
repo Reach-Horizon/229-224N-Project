dddacd39abc08336d9d41dc78bb7e0c3cab13134
Victor Zhong <victor@victorzhong.com>
2014-11-25 09:54:56 -0500

    initial language model

---
+++ features/nlang.py
@@ -0,0 +1,39 @@
+from gensim.models import Word2Vec
+from nltk import word_tokenize, sent_tokenize
+import sys, os
+
+root_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(root_dir)
+
+from util.common import extract_code_sections
+from util.DataStreamer import DataStreamer
+
+class MyExamples(object):
+    def __init__(self, examples):
+        self.examples = examples
+    def __iter__(self):
+        for example in self.examples:
+            code, noncode = extract_code_sections(example.data['body'])
+            for sentence in sent_tokenize(noncode):
+                yield word_tokenize(sentence.lower())
+
+def train_model(examples):
+    generator = MyExamples(examples)
+    model = Word2Vec(generator) # this commences the training
+    return model
+
+
+
+
+if __name__ == '__main__':
+    train_file = os.path.join(root_dir, 'full_data', 'Train.csv.bz2')
+    examples_generator = DataStreamer.load_from_file(train_file)
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    model = train_model(examples_generator)
+    out_file = os.path.join(root_dir, 'full_data', 'nlang')
+    model.save(out_file)
+
+
+
+
