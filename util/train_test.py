Fix bug for computing average f1 and recall---it was printing average precision before!

util/train_test.py
@@ -3,7 +3,7 @@ from sklearn.decomposition import PCA
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 import argparse, os, sys
 from time import time
 from pprint import pprint
@@ -35,14 +35,15 @@ for k in range(Ytrain.shape[1]):
     print("Training class %s ..." % k)
 
     # get training examples
-    X, Y = get_dataset_for_class(k, Xtrain, Ytrain, fair_sampling=True, restrict_sample_size=1000)
+    #X, Y = get_dataset_for_class(k, Xtrain, Ytrain, fair_sampling=True, restrict_sample_size=1000)
+    X, Y = get_dataset_for_class(k, Xtrain, Ytrain, fair_sampling=False)
 
     pipeline = Pipeline([
         ('kbest', SelectKBest(chi2, k=1000)),
         ('tfidf', TfidfTransformer(use_idf=False, norm='l1')),
         ('densifier', DenseMatrixTransformer()),
         ('pca', PCA(n_components=500)),
-        ('clf', SVC(gamma=0.1, C=1000)), #RBF kernel
+        ('clf', LinearSVC(C=1000)),
     ])
 
     print("pipeline:", [name for name, _ in pipeline.steps])
@@ -62,7 +63,8 @@ for k in range(Ytrain.shape[1]):
 
     train_scores += [scores]
 
-    X, Y = get_dataset_for_class(k, Xtest, Ytest, fair_sampling=True)
+    #X, Y = get_dataset_for_class(k, Xtest, Ytest, fair_sampling=True)
+    X, Y = get_dataset_for_class(k, Xtest, Ytest, fair_sampling=False)
     Ypred = pipeline.predict(X)
     scores = {
         'accuracy': accuracy_score(Y, Ypred),
@@ -78,15 +80,15 @@ for k in range(Ytrain.shape[1]):
 train_ave = {
     'accuracy': np.mean([d['accuracy'] for d in train_scores]),
     'precision': np.mean([d['precision'] for d in train_scores]),
-    'recall': np.mean([d['precision'] for d in train_scores]),
-    'f1': np.mean([d['precision'] for d in train_scores]),
+    'recall': np.mean([d['recall'] for d in train_scores]),
+    'f1': np.mean([d['f1'] for d in train_scores]),
 }
 
 test_ave = {
     'accuracy': np.mean([d['accuracy'] for d in test_scores]),
     'precision': np.mean([d['precision'] for d in test_scores]),
-    'recall': np.mean([d['precision'] for d in test_scores]),
-    'f1': np.mean([d['precision'] for d in test_scores]),
+    'recall': np.mean([d['recall'] for d in test_scores]),
+    'f1': np.mean([d['f1'] for d in test_scores]),
 }
 
 print 'Train average:'