a7bba5d84ddeeee860725431eab87957775f5765
Victor Zhong <victor@victorzhong.com>
2014-11-30 14:18:19 -0800

    Just tune the classifier

--- util/tune_hyper.py
+++ util/tune_hyper.py
@@ -20,62 +20,17 @@ from features.extractors import *
 from features.transformers import DenseMatrixTransformer
 
 parser = argparse.ArgumentParser(description = 'does hyperparameter tuning')
-parser.add_argument('trainExamplesZip', type = str, help = 'features matrix for training examples')
+parser.add_argument('trainFeatures', type = str, help = 'features matrix for training examples')
 parser.add_argument('trainLabels', type = str, help = 'labels file for training pipeline')
 parser.add_argument('out_file', help='where to store the best settings (json)')
 parser.add_argument('-p', '--parallel', type=int, help='the number of jobs to run in parallel. Default=1', default=1)
 args = parser.parse_args()
 
 
-examples = [e for e in DataStreamer.load_from_bz2(args.trainExamplesZip)]
+X = load_sparse_csr(args.trainFeatures)
 Y = load_sparse_csr(args.trainLabels).todense()
 
 pipeline = Pipeline([
-    ('ngrams', FeatureUnion([
-        ('title', Pipeline([
-            ('counts', TitleNgramsExtractor(
-                ngram_range=(1,1),
-                binary=True,
-                max_df=1.0,
-            )),
-            ('kbest', SelectKBest(chi2, k=100)),
-            ('tfidf', TfidfTransformer(use_idf=True, norm='l2')),
-        ])),
-        ('body', Pipeline([
-            ('counts', BodyNgramsExtractor(
-                ngram_range=(1,1),
-                binary=False,
-                max_df=1.0,
-            )),
-            ('kbest', SelectKBest(chi2, k=100)),
-            ('tfidf', TfidfTransformer(use_idf=False, norm='l1')),
-        ])),
-        ('code', Pipeline([
-            ('counts', CodeNgramsExtractor(
-                ngram_range=(1,1),
-                binary=True,
-                max_df=0.5,
-            )),
-            ('kbest', SelectKBest(chi2, k=500)),
-            ('tfidf', TfidfTransformer(use_idf=True, norm='l2')),
-        ])),
-        ('pygment', Pipeline([
-            ('counts', PygmentExtractor(
-                ngram_range=(1,1),
-                binary=True,
-            )),
-            ('kbest', SelectKBest(chi2, k=50)),
-            ('tfidf', TfidfTransformer(use_idf=True, norm='l2')),
-        ])),
-        ('label', Pipeline([
-            ('counts', LabelCountsExtractor(
-                ngram_range=(1,1),
-                binary=True,
-            )),
-            ('kbest', SelectKBest(chi2, k=10000)),
-            ('tfidf', TfidfTransformer(use_idf=True, norm='l2')),
-        ])),
-    ])),
     ('densifier', DenseMatrixTransformer()),
     ('pca', PCA()),
     ('clf', OneVsRestClassifier(LogisticRegression(class_weight='auto'))),
@@ -83,18 +38,18 @@ pipeline = Pipeline([
 
 parameters = {
     'pca__n_components': (30, 100, 300, 1000),
-    'clf__C': 10. ** np.arange(1, 4),
+    'clf__estimator__C': 10. ** np.arange(1, 4),
     #'clf__gamma': 10. ** np.arange(-2, 1),
 }
 
-searcher = RandomizedSearchCV(pipeline, parameters, n_jobs=args.parallel, verbose=1, n_iter=10)
+searcher = GridSearchCV(pipeline, parameters, n_jobs=args.parallel, verbose=1)
 
 print(searcher)
 print("pipeline:", [name for name, _ in pipeline.steps])
 print("parameters:")
 pprint(parameters)
 t0 = time()
-searcher.fit(examples, Y)
+searcher.fit(X, Y)
 print("done in %0.3fs" % (time() - t0))
 print()
 
