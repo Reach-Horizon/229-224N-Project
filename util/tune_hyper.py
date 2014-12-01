8a406535b4b083d4307a6b75ff647ccbe1796628
Victor Zhong <victor@victorzhong.com>
2014-11-30 18:51:44 -0800

    default settings

--- util/tune_hyper.py
+++ util/tune_hyper.py
@@ -26,7 +26,7 @@ parser = argparse.ArgumentParser(description = 'does hyperparameter tuning')
 parser.add_argument('trainFeatures', type = str, help = 'features matrix for training examples')
 parser.add_argument('trainLabels', type = str, help = 'labels file for training pipeline')
 parser.add_argument('out_file', help='where to store the best settings (json)')
-parser.add_argument('-p', '--parallel', type=int, help='the number of jobs to run in parallel. Default=1', default=1)
+parser.add_argument('--n_jobs', type=int, help='the number of jobs to run in parallel. Default=1', default=1)
 args = parser.parse_args()
 
 
@@ -34,19 +34,20 @@ X = load_sparse_csr(args.trainFeatures).toarray()
 Y = load_sparse_csr(args.trainLabels, dtype=np.uint8).toarray()
 
 pipeline = Pipeline([
-    ('clf', OneVsRestClassifier(RandomForestClassifier(), n_jobs=args.parallel)),
+    ('clf', OneVsRestClassifier(LogisticRegression(), n_jobs=args.n_jobs)),
 ])
 
 parameters = {
-    "clf__estimator__max_depth": [3, None],
-    "clf__estimator__max_features": range(1, 11),
-    "clf__estimator__min_samples_split": range(1, 11),
-    "clf__estimator__min_samples_leaf": range(1, 11),
-    "clf__estimator__bootstrap": [True, False],
-    "clf__estimator__criterion": ["gini", "entropy"],
+    "clf__estimator__C": [10, 100]
+    # "clf__estimator__max_depth": [3, None],
+    # "clf__estimator__max_features": range(1, 11),
+    # "clf__estimator__min_samples_split": range(1, 11),
+    # "clf__estimator__min_samples_leaf": range(1, 11),
+    # "clf__estimator__bootstrap": [True, False],
+    # "clf__estimator__criterion": ["gini", "entropy"],
 }
 
-searcher = RandomizedSearchCV(pipeline, parameters, scoring=make_scorer(f1_score), n_jobs=args.parallel, verbose=1, cv=KFold(X.shape[0]), n_iter=10) #default StratifiedKFold doesn't work with multiclass classification
+searcher = GridSearchCV(pipeline, parameters, scoring=make_scorer(f1_score), n_jobs=args.n_jobs, verbose=1, cv=KFold(X.shape[0])) #default StratifiedKFold doesn't work with multiclass classification
 
 print(searcher)
 print("pipeline:", [name for name, _ in pipeline.steps])
