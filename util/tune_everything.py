38f63870209e40549a939495cebc72498b1ccdfa
Victor Zhong <victor@victorzhong.com>
2014-12-02 13:46:13 -0800

    best settings

--- util/tune_everything.py
+++ util/tune_everything.py
@@ -24,7 +24,7 @@ from features.extractors import *
 from features.transformers import DenseMatrixTransformer
 
 parser = argparse.ArgumentParser(description = 'does hyperparameter tuning')
-parser.add_argument('trainExamples', type = str, help = 'features matrix for training examples')
+parser.add_argument('trainExamplesZip', type = str, help = 'features matrix for training examples')
 parser.add_argument('trainLabels', type = str, help = 'labels file for training pipeline')
 parser.add_argument('out_file', help='where to store the best settings (json)')
 parser.add_argument('-p', '--parallel', type=int, help='the number of jobs to run in parallel. Default=1', default=10)
@@ -37,64 +37,59 @@ Y = load_sparse_csr(args.trainLabels, dtype=np.uint8).toarray()
 pipeline = Pipeline([
     ('ngrams', FeatureUnion([
         ('title', Pipeline([
-            ('counts', TitleNgramsExtractor()),
-            ('kbest', SelectKBest(chi2)),
-            ('tfidf', TfidfTransformer()),
-        ])),
+            ('counts', TitleNgramsExtractor(binary=True)),
+            ('tfidf', TfidfTransformer(norm='l2', use_idf=True)),
+            ('kbest', SelectKBest(chi2, k=100)),
+            ])),
         ('body', Pipeline([
-            ('counts', BodyNgramsExtractor()),
-            ('kbest', SelectKBest(chi2)),
-            ('tfidf', TfidfTransformer()),
+            ('counts', BodyNgramsExtractor(binary=False)),
+            ('tfidf', TfidfTransformer(use_idf=True, norm='l2')),
+            ('kbest', SelectKBest(chi2, k=1000)),
         ])),
-        ('code', Pipeline([
-            ('counts', CodeNgramsExtractor()),
-            ('kbest', SelectKBest(chi2)),
-            ('tfidf', TfidfTransformer()),
-        ])),
+        #('code', Pipeline([
+        #    ('counts', CodeNgramsExtractor()),
+        #    ('tfidf', TfidfTransformer()),
+        #    ('kbest', SelectKBest(chi2)),
+        #])),
         ('pygment', Pipeline([
-            ('counts', PygmentExtractor()),
-            ('kbest', SelectKBest(chi2)),
-            ('tfidf', TfidfTransformer()),
+            ('counts', PygmentExtractor(binary=True)),
+            ('tfidf', TfidfTransformer(use_idf=True)),
+            ('kbest', SelectKBest(chi2, k=100)),
         ])),
         ('label', Pipeline([
-            ('counts', LabelCountsExtractor()),
-            ('kbest', SelectKBest(chi2)),
-            ('tfidf', TfidfTransformer()),
+            ('counts', LabelCountsExtractor(binary=True)),
+            ('tfidf', TfidfTransformer(use_idf=True)),
         ])),
     ])),
-    ('clf', OneVsRestClassifier(RandomForestClassifier(), n_jobs=args.parallel)),
+    ('clf', OneVsRestClassifier(LinearSVC(class_weight='auto', loss='l1', C=5), n_jobs=args.parallel)),
 ])
 
 parameters = {
-    'ngrams__title__counts__max_df': (0.5, 1.0),
-    'ngrams__title__counts__max_features': (None, 1000, 10000, 100000),
-    'ngrams__title__counts__binary': (True, False),
-    'ngrams__title__kbest__k': (100, 500, 1000),
-    'ngrams__title__tfidf__use_idf': (True, False),
-    'ngrams__title__tfidf__norm': ('l1', 'l2'),
-    'ngrams__body__counts__max_df': (0.5, 1.0),
-    'ngrams__body__counts__binary': (True, False),
-    'ngrams__body__kbest__k': (1000, 10000, 100000),
-    'ngrams__body__tfidf__use_idf': (True, False),
-    'ngrams__body__tfidf__norm': ('l1', 'l2'),
-    'ngrams__code__counts__max_df': (0.5, 0.75, 1.0),
-    'ngrams__code__counts__binary': (True, False),
-    'ngrams__code__kbest__k': (100, 500, 1000),
-    'ngrams__code__tfidf__use_idf': (True, False),
-    'ngrams__code__tfidf__norm': ('l1', 'l2'),
-    'ngrams__pygment__counts__binary': (True, False),
-    'ngrams__pygment__kbest__k': (50, 200, 400),
-    'ngrams__pygment__tfidf__use_idf': (True, False),
-    'ngrams__pygment__tfidf__norm': ('l1', 'l2'),
-    'ngrams__label__counts__binary': (True, False),
-    'ngrams__label__kbest__k': (100, 1000, 10000),
-    'ngrams__label__tfidf__use_idf': (True, False),
-    'ngrams__label__tfidf__norm': ('l1', 'l2'),
-    "clf__estimator__C": [10, 100, 1000],
-    "clf__estimator__loss": ['l1', 'l2'],
+    #'ngrams__title__counts__binary': (True, False),
+    #'ngrams__title__kbest__k': (100, 1000, 'all'),
+    #'ngrams__title__tfidf__use_idf': (True, False),
+    #'ngrams__title__tfidf__norm': ('l1', 'l2'),
+    #'ngrams__body__counts__binary': (True, False),
+    #'ngrams__body__kbest__k': (100, 1000, 'all'),
+    #'ngrams__body__tfidf__use_idf': (True, False),
+    #'ngrams__body__tfidf__norm': ('l1', 'l2'),
+    #'ngrams__code__counts__binary': (True, False),
+    #'ngrams__code__kbest__k': (100, 10000, 'all'),
+    #'ngrams__code__tfidf__use_idf': (True, False),
+    #'ngrams__code__tfidf__norm': ('l1', 'l2'),
+    #'ngrams__pygment__kbest__k': (100, 1000, 'all'),
+    #'ngrams__pygment__tfidf__use_idf': (True, False),
+    #'ngrams__pygment__kbest__k': (100, 1000, 'all'),
+    #'ngrams__label__counts__binary': (True, False),
+    #'ngrams__label__kbest__k': (100, 1000, 'all'),
+    #'ngrams__label__tfidf__use_idf': (True, False),
+    #"clf__estimator__C": range(1, 31),
+    #"clf__estimator__loss": ('l1', 'l2'),
 }
 
-searcher = RandomizedSearchCV(pipeline, parameters, scoring = make_scorer(f1_score), n_jobs=args.parallel, verbose=1, cv=KFold(len(examples)), n_iter=200) #default StratifiedKFold doesn't work with multiclass classification
+searcher = GridSearchCV(pipeline, parameters, scoring =
+                              make_scorer(f1_score), n_jobs=args.parallel,
+                              verbose=1, cv=KFold(len(examples))) #default StratifiedKFold doesn't work with multiclass classification
 
 print(searcher)
 print("pipeline:", [name for name, _ in pipeline.steps])
